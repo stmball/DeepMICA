8f90a76040f6c81435cc18a5f116667501e5144f
Sam Ball <stmball@gmail.com>
2020-11-30 12:43:58 +0000

    deleting some files from git tree

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""
-    DeepMICA is a toolkit for generating, idealising, analysing and visualising Markovian (or Kinetic) Ion Channel data models for Deep Learning models.
-"""
-
-__email__ = 'sgsball@liverpool.ac.uk'
-__author__ = 'Samuel Ball'
-
